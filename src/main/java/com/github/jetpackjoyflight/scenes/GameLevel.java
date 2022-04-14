f6390d71de8eefebc191d9f5f33f75b2f7efef9d
Niels Kolkman <niels.kolkman6@gmail.com>
2022-04-14 19:42:04 +0200

    cleanup

--- src/main/java/com/github/jetpackjoyflight/scenes/GameLevel.java
+++ src/main/java/com/github/jetpackjoyflight/scenes/GameLevel.java
@@ -5,12 +5,9 @@ import com.github.hanyaeger.api.EntitySpawnerContainer;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.api.scenes.TileMapContainer;
 import com.github.jetpackjoyflight.entities.Player;
-import com.github.jetpackjoyflight.entities.Sharky;
-import com.github.jetpackjoyflight.entities.map.CoralTileMap;
 import com.github.jetpackjoyflight.entities.rocket.Rocket;
 import com.github.jetpackjoyflight.entities.text.BubblesPoppedText;
 import com.github.jetpackjoyflight.entities.text.HealthText;
-import com.github.jetpackjoyflight.spawners.BubbleSpawner;
 import com.github.jetpackjoyflight.Main;
 
 public class GameLevel extends DynamicScene implements EntitySpawnerContainer, TileMapContainer {
