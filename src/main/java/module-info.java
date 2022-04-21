b68395235d32408153c37a0ad980a5333cd2e864
jeroendn <51778824+jeroendn@users.noreply.github.com>
2022-04-21 10:30:27 +0200

    Oops

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module jetpackjoyflight {
     requires hanyaeger;
 
     exports com.github.jetpackjoyflight;
-    exports com.github.jetpackjoyflight.entities.map;
 
     opens audio;
     opens backgrounds;
